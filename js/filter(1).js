23589a9ad9d097f0e463c2239a4dc6c5130d32b9
didarzhumabekuly <120437186+didarzhumabekuly@users.noreply.github.com>
2023-04-27 11:06:53 +0600

    Add files via upload

---
+++ js/filter(1).js
@@ -0,0 +1,56 @@
+const families = [{
+	id: 'rec43w3ipXvP28vog',
+	title: 'Alash 2049',
+	family: 'Awesome collection',
+	image: './image/awes1.png',
+},
+{
+	id: 'rec4f2RIftFCb7aHh',
+	title: 'Alash ziyalilary',
+	family: 'Awesome collection',
+	image: '/img/awes2.png',
+},
+{
+	id: 'rec8kkCmSiMkbkiko',
+	title: 'Alash Karta',
+	family: 'Awesome collection',
+	image: '/img/awes3.png',
+},
+{
+	id: 'recNWGyP7kjFhSqw3',
+	title: 'Kazakh woman',
+	family: 'Gorgeous collection',
+	image: '/img/collection main2.png',
+},
+{
+	id: 'recZEougL5bbY4AEx',
+	title: 'Kazakh Hokage',
+	family: 'Gorgeous collection',
+	image: '/img/hokage.png',
+},
+
+{
+	id: 'recjMK1jgTb2ld7sv',
+	title: 'Kiyz ui',
+	family: 'Gorgeous collection',
+	image: '/img/awes4.png',
+},
+{
+	id: 'recjMK1jgTb2ld7sv',
+	title: 'Shanyrak',
+	family: 'Gorgeous collection',
+	image: '/img/awes5.png',
+},
+{
+	id: 'recxaXFy5IW539sgM',
+	title: 'Kazakhstan tuy',
+	family: 'Amazing collection',
+	image: '/img/awes6.png',
+},
+{
+	id: 'recyqtRglGNGtO4Q5',
+	title: 'Qorqyt ata',
+	family: 'Amazing collection',
+	image: '/img/top8.jpg',
+},
+];
