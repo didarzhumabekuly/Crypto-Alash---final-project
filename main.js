e1ad2d7cdee980bebc4aa084c3549d3e5c5ce718
didarzhumabekuly <120437186+didarzhumabekuly@users.noreply.github.com>
2023-04-27 10:44:45 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+const btnDarkMode = document.querySelector(".dark-mode-btn");
+
+
+if (localStorage.getItem('darkMode') === 'dark') {
+	btnDarkMode.classList.add("dark-mode-btn--active");
+	document.body.classList.add("dark");
+}
+
+// включение ночного режима по кнопке
+btnDarkMode.onclick = function () {
+	btnDarkMode.classList.toggle("dark-mode-btn--active");
+	const isDark = document.body.classList.toggle('dark');
+
+	if (isDark) {
+	localStorage.setItem('darkMode','dark');
+	} else {
+	localStorage.setItem("darkMode","light");
+	}
+}
